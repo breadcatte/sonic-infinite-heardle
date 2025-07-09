5b8e246eb1a8faec821da586b53e84a861934941
Beignet <nterrien0@gmail.com>
2025-07-09 23:25:09 +0200

    remove createArchive

--- createArchive.sh
+++
@@ -1,6 +0,0 @@
-version="V4.5"
-
-filename="etrian-infinite-heardle_"$version
-
-git archive --prefix=$filename/ --format=zip --output=$filename.zip -9 HEAD
-mv $filename.zip ./archive/$filename.zip
